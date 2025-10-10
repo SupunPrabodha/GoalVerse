32bcc155a9be63ca538b3285a62f0c116d2cb51a
Supun Prabodha <supunprabodha0915@gmail.com>
2025-10-10 11:15:57 +0530

    initial

--- mobile/lib/api.js
+++ mobile/lib/api.js
@@ -2,7 +2,7 @@
 // If you test on a physical phone, use your PC's LAN IP (e.g. 192.168.1.10).
 import axios from "axios";
 
-export const API_BASE_URL = "http://172.24.47.164:4000/api"; // <= change this
+export const API_BASE_URL = "http://10.95.61.40:4000/api"; // <= change this
 export const api = axios.create({ baseURL: API_BASE_URL });
 
 // helper to attach token
