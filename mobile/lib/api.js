b52dd0d510478ce820dfd928e29909bcd6417a76
DishanGayantha <162087563+DishanGayantha@users.noreply.github.com>
2025-10-13 10:11:57 +0530

    v 4.4

--- mobile/lib/api.js
+++ mobile/lib/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_BASE_URL = "https://goalverse.onrender.com//api"; // <- set to your laptop's IP
+export const API_BASE_URL = "http://172.24.47.164:4000/api"; // <- set to your laptop's IP
 export const api = axios.create({ baseURL: API_BASE_URL });
 
 // helper to attach token
